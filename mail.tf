c2e733ddb825fc7c7431b70dccdc8d8140d5e4e7
vijay.patel <to.patelvijay@gmail.com>
2019-02-22 09:23:55 +0530

    small typo correction

--- mail.tf
+++ mail.tf
@@ -6,7 +6,7 @@ locals {
   ingress_from_port = "${var.ingress_from_port}"
   ingress_to_port   = "${var.ingress_to_port}"
   ingress_protocol  = "${var.ingress_protocol}"
-git }
+ }
 
 resource "aws_security_group" "allow_all" {
   name        = "${var.security_group_name}"
